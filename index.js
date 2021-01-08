8c1f330528160518a5f1f049a9038a5b3f45026e
jessicamhunt <j.m.hunt1018@gmail.com>
2021-01-07 19:20:33 -0500

    remove toggle logic and simplify code to reveal answer only once.

--- index.js
+++ index.js
@@ -26,9 +26,5 @@ function showDivs(n) {
 
 function revealAnswer() {
   var x = document.getElementById("answer");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+  x.style.display = "block";
 }
